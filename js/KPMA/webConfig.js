2999855c4ebd66d95c6a31871cc9b6277b054538
sedo-games <sedo.games@gmail.com>
2017-02-25 16:17:53 +0200

    kpma rate true

--- js/KPMA/webConfig.js
+++ js/KPMA/webConfig.js
@@ -4,5 +4,5 @@
     "ChartboostFullScreenEnabled": false,
     "AdCashInterstitialEnabled": false,
     "RevMobVideoEnabled": false,
-    "RateStarEnabled": true,    
+    "RateStarEnabled": true    
 }
